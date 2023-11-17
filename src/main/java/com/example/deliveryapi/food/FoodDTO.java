51f569a442dcda7426745721721dce4e4ffdd9b8
junghwan <jhzzang97@gmail.com>
2023-11-18 08:32:37 +0900

    price 필드 데이터 타입 수정

--- src/main/java/com/example/deliveryapi/food/FoodDTO.java
+++ src/main/java/com/example/deliveryapi/food/FoodDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Builder
 public class FoodDTO {
     private String name;
-    private int price;
+    private Integer price;
     private String description;
     private String belong;
 
