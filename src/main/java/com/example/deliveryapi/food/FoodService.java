368b562646ed6dc2c21a9aa0342c1c9ff76f8b0a
junghwan <jhzzang97@gmail.com>
2023-11-24 09:20:26 +0900

    REFACTOR : FoodService 코드 구조 개선

--- src/main/java/com/example/deliveryapi/food/FoodService.java
+++ src/main/java/com/example/deliveryapi/food/FoodService.java
@@ -1,9 +1,12 @@
 package com.example.deliveryapi.food;
 
+import com.example.deliveryapi.food.dto.PostFoodRequest;
+import com.example.deliveryapi.food.dto.UpdateFoodRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class FoodService {
@@ -11,26 +14,34 @@ public class FoodService {
     private final FoodJpaRepository foodJpaRepository;
 
     @Autowired // 생성자가 하나만 있을 경우 생략 가능
-    public FoodService (FoodJpaRepository foodJpaRepository){
+    public FoodService(FoodJpaRepository foodJpaRepository) {
         this.foodJpaRepository = foodJpaRepository;
     }
 
-    public FoodDTO selectFoodById(int id) {
-        Optional<Food> food = foodJpaRepository.findById(id);
-        return FoodDTO.fromEntity(food.orElse(null));
+    public List<Food> findAll(){
+        return foodJpaRepository.findAll();
     }
 
-    public void postFood(FoodDTO dto) {
+    public Food findById(long id){
+        return foodJpaRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found : " + id));
+    }
+
+    public Food postFood(PostFoodRequest dto) {
         Food food = dto.toEntity();
-        foodJpaRepository.save(food);
+        return foodJpaRepository.save(food);
     }
 
-    public void updateFood(int id, FoodDTO dto) {
-        Food food = dto.toEntity();
-        foodJpaRepository.save(food);
+    @Transactional
+    public Food update(long id, UpdateFoodRequest request){
+        Food food = foodJpaRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found" + id));
+
+        food.update(request.getName(), request.getPrice(), request.getDescription(), request.getBelong());
+        return food;
     }
 
-    public void deleteFood(int id) {
+    public void deleteFood(long id) {
         foodJpaRepository.deleteById(id);
     }
 }
